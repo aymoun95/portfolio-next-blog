e9ab0ad9aade5e30ccc831807f68a7be4a6dcc06
a_zlaouia <A.zlauia@wssel.com>
2021-05-18 23:34:25 +0100

    implmeent the blog markdown parser

---
+++ pages/blog/[slug].js
@@ -0,0 +1,31 @@
+import { getFiles, getFileBySlug } from "../../lib/mdx";
+import BlogLayout from "../../layouts/blog";
+import MDXComponents from "../../components/MDXComponents";
+import { MDXRemote } from "next-mdx-remote";
+
+export default function Blog({ mdxSource, frontMatter }) {
+  return (
+    <BlogLayout frontMatter={frontMatter}>
+      <MDXRemote {...mdxSource} components={MDXComponents} />
+    </BlogLayout>
+  );
+}
+
+export async function getStaticPaths() {
+  const posts = await getFiles("blog");
+
+  return {
+    paths: posts.map((p) => ({
+      params: {
+        slug: p.replace(/\.md/, ""),
+      },
+    })),
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params }) {
+  const post = await getFileBySlug("blog", params.slug);
+
+  return { props: post };
+}
