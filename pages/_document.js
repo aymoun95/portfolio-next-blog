fea18203434d898a2c3c1c1fe03f4691e7b93293
a_zlaouia <A.zlauia@wssel.com>
2021-09-03 11:28:57 +0100

    remove tag filter search bar and add all favicon

--- pages/_document.js
+++ pages/_document.js
@@ -20,11 +20,6 @@ export default class MyDocument extends NextDocument {
             type="image/x-icon"
           /> */}
           <link
-            rel="apple-touch-icon"
-            sizes="180x180"
-            href="/images/favicon/apple-touch-icon.png"
-          />
-          <link
             rel="icon"
             type="image/png"
             sizes="32x32"
@@ -36,6 +31,18 @@ export default class MyDocument extends NextDocument {
             sizes="16x16"
             href="/images/favicon/favicon-16x16.png"
           />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/images/favicon/apple-touch-icon.png"
+          />
+          <link rel="manifest" href="/site.webmanifest" />
+          <link
+            rel="mask-icon"
+            href="/images/favicon/safari-pinned-tab.svg"
+            color="#5bbad5"
+          />
+
           <meta
             name="description"
             content="A portfolio blog for me 'Aymen Ben Zlaouia' where I showcase the projects that I have worked on, a blog where I learn new things by teaching people the things that I have learnt and  a contact form if someone would like to reach me to correct me something on a blog or hire me for a work or even thank me for the help."
